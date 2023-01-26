532b5c0fb1e683a20d3a303ef1c8707747ada395
Richard Kruse <richardkruse3rd@gmail.com>
2023-01-26 21:54:08 +0900

    initial commit

---
+++ portfolio_site/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+//const element = <FontAwesomeIcon icon={faEnvelope} />
+
+
+
+
+export const Header = () => {
+  return (
+    <div>
+    Header
+    <FontAwesomeIcon icon={faEnvelope} />
+    Header
+    </div>
+
+  )
+}
